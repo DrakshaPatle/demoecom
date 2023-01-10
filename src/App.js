ac7fa0dee668ba733be6bc3c8087d25149a3eeba
DrakshaPatle <draksha@appsdeployer.com>
2023-01-10 19:27:48 +0530

    add

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { useState } from "react"
 import "./App.css"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Header from "./common/header/Header"
-import Pages from "./pages/Pages"
+ import Pages from "./pages/Pages"
 import Data from "./components/Data"
 import Cart from "./common/Cart/Cart"
 import Footer from "./common/footer/Footer"
-import Sdata from "./components/shops/Sdata"
+ import Sdata from "./components/shops/Sdata"
 
 function App() {
   /*
--- src/components/MainPage/Categories.jsx
+++ src/components/MainPage/Categories.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+
+
 const Categories = () => {
   const data = [
     {
